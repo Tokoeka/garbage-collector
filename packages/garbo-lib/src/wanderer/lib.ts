3e1a801c33e768b74fe12c8135d9efd372dde355
Tokoeka <nzgamer666@gmail.com>
2025-01-06 12:59:11 +0000

    Holiday Islands are Bad Parents

--- packages/garbo-lib/src/wanderer/lib.ts
+++ packages/garbo-lib/src/wanderer/lib.ts
@@ -125,6 +125,7 @@ const ILLEGAL_PARENTS = [
   "Psychoses",
   "PirateRealm",
   "A Monorail Station",
+  "Holiday Islands",
 ];
 const ILLEGAL_ZONES = ["The Drip", "Suburbs"];
 const canAdventureOrUnlockSkipList = [
