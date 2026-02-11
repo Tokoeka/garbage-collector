e19ce4fba43d38f052dcdc8956cb10a1196ee67a
Kiwi Tokoeka <nz.gamer.666@gmail.com>
2026-02-11 12:09:54 +0000

    refix MeatTargetValue

--- packages/garbo/src/lib.ts
+++ packages/garbo/src/lib.ts
@@ -150,8 +150,8 @@ export function modeValueOfItem(mode: BonusEquipMode): number {
 export const WISH_VALUE = 50000;
 export const HIGHLIGHT = isDarkMode() ? "yellow" : "blue";
 export const ESTIMATED_OVERDRUNK_TURNS = 50;
-export const MEAT_TARGET_MULTIPLIER = (): number =>
-  globalOptions.prefs.meatTargetMultiplier;
+export const MEAT_TARGET_VALUE = (): number =>
+  globalOptions.prefs.meatTargetValue;
 
 export const propertyManager = new PropertiesManager();
 
